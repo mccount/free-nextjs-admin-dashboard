05e66bdb827b95c45669d1fa26dc13236fafe88d
Jannik <roll@lw-datentechnik.de>
2023-10-02 22:47:28 +0200

    small changes

--- components/Checkboxes/CheckboxThree.tsx
+++ components/Checkboxes/CheckboxThree.tsx
@@ -1,7 +1,69 @@
+// "use client";
+// import { useState } from 'react';
+// import createUUID from '../helpers/createUUID';
+
+interface CheckBox {
+  checkedOnInit?: boolean,
+  text?: string,
+  initId?: string
+}
+
+// const CheckboxThree = ({checkedOnInit, text, initId = createUUID()} : CheckBox) => {
+//   const [isChecked, setIsChecked] = useState<boolean>(checkedOnInit);
+
+//   return (
+//     <div>
+//       <label
+//         htmlFor="checkboxLabelThree"
+//         className="flex cursor-pointer select-none items-center"
+//       >
+//         <div className="relative">
+//           <input
+//             type="checkbox"
+//             id={initId}
+//             className="sr-only"
+//             onChange={() => {
+//               console.log("setting " + (!isChecked))
+//               setIsChecked(!isChecked);
+//             }}
+//           />
+//           <div
+//             className={`box mr-4 flex h-5 w-5 items-center justify-center rounded border ${
+//               isChecked && 'border-primary bg-gray dark:bg-transparent'
+//             }`}
+//           >
+//             <span
+//               className={`text-primary opacity-0 ${
+//                 isChecked && '!opacity-100'
+//               }`}
+//             >
+//               <svg
+//                 className="h-3.5 w-3.5 stroke-current"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 xmlns="http://www.w3.org/2000/svg"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth="2"
+//                   d="M6 18L18 6M6 6l12 12"
+//                 ></path>
+//               </svg>
+//             </span>
+//           </div>
+//         </div>
+//         {text ? text : ""}
+//       </label>
+//     </div>
+//   );
+// };
+
+
 "use client";
 import { useState } from 'react';
 
-const CheckboxThree = () => {
+const CheckboxThree = ({checkedOnInit = false, text = "", initId = "temp"} : CheckBox) => {
   const [isChecked, setIsChecked] = useState<boolean>(false);
 
   return (
@@ -50,5 +112,6 @@ const CheckboxThree = () => {
     </div>
   );
 };
+
 
 export default CheckboxThree;
